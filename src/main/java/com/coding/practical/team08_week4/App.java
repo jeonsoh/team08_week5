7456578e5309e9bbf99c9d225c7e9c6f2816ab52
sh92 <sanghee.lee1992@gmail.com>
2016-03-31 22:39:28 +0900

    my first maven

---
+++ src/main/java/com/coding/practical/team08_week4/App.java
@@ -0,0 +1,13 @@
+package com.coding.practical.team08_week4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

a5048a94f6a7e2ec64149c06106f18814faa1a71
SoHyang.Jeon <SoHyang.Jeon126@gmail.com>
2016-04-01 18:50:17 +0900

    User get set 추가

--- src/main/java/com/practical/billviewservice/App.java
+++ src/main/java/com/practical/billviewservice/App.java
@@ -10,10 +10,10 @@ public class App {
 		
 	}
     public static void main( String[] args ){
+    
     	User myUser = new User("Gold",2000,3,"Gill-Dong");
-    	Plan myPlan = new Gold();
-    	Calculator myCalculator = new Calculator(myUser,myPlan);
-    	
+        Plan myPlan = new Gold();
+    	Calculator myCalculator = new Calculator(myUser,myPlan); 	
     	BillViewService billviewsystem = new BillViewService(myUser, myPlan, myCalculator);
     	billviewsystem.showUser();
     	billviewsystem.showPlan();

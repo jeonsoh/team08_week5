7892e8084ccd638e7d72696cc35cd8f12004a0bb
SoHyang.Jeon <SoHyang.Jeon126@gmail.com>
2016-04-01 19:33:18 +0900

    Silver/Gold 추가

--- src/main/java/com/practical/billviewservice/Silver.java
+++ src/main/java/com/practical/billviewservice/Silver.java
@@ -1,5 +1,33 @@
 package com.practical.billviewservice;
 
 public class Silver extends Plan{
-
+	private double Basic_Montly_Rate = 29.95;
+	private int Included_Minutes = 500;
+	private double Rate_Per_Excess_Minute = 0.54;
+	private double Additional_Line_Rate = 21.50;
+	private int Family_discount_Basic =3;
+	private double Family_discount_Basic_Rate = 5.00;
+	
+	public double getBasic_Montly_Rate() {
+		return Basic_Montly_Rate;
+	}	
+	public int getIncluded_Minutes() {
+		return Included_Minutes;
+	}	
+	public double getRate_Per_Excess_Minute() {
+		return Rate_Per_Excess_Minute;
+	}	
+	public double getAdditional_Line_Rate() {
+		return Additional_Line_Rate;
+	}	
+	public int getFamily_discount_Basic() {
+		return Family_discount_Basic;
+	}	
+	public double getFamily_discount_Basic_Rate() {
+		return Family_discount_Basic_Rate;
+	}
+	
+	
+	
+	
 }

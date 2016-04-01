a8052ccfe4153379899ce3bf24e25e0e95d1ff72
sh92 <sanghee.lee1992@gmail.com>
2016-04-02 00:23:23 +0900

    field명 method명 변경 소문자시작으로 그리고 _제

--- src/main/java/com/practical/billviewservice/Plan.java
+++ src/main/java/com/practical/billviewservice/Plan.java
@@ -2,10 +2,10 @@ package com.practical.billviewservice;
 
 interface Plan {
 	
-	public double getBasic_Montly_Rate();
-	public int getIncluded_Minutes();
-	public double getRate_Per_Excess_Minute();
-	public double getAdditional_Line_Rate();
-	public int getFamily_discount_Basic();
-	public double getFamily_discount_Basic_Rate();
+	public double getBasicMontlyRate();
+	public int getIncludedMinutes();
+	public double getRatePerExcessMinute();
+	public double getAdditionalLineRate();
+	public int getFamilyDiscountBasic();
+	public double getFamilyDiscountBasicRate();
 }

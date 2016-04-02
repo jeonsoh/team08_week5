f42aa7f76c6c130bf74235b04afeac9950b3c9e1
sh92 <sanghee.lee1992@gmail.com>
2016-04-02 09:01:56 +0900

    white -> space 했는데 sonarqube에서 왜 사라지지 않지..

--- src/main/java/com/practical/billviewservice/User.java
+++ src/main/java/com/practical/billviewservice/User.java
@@ -1,49 +1,40 @@
 package com.practical.billviewservice;
 
 public class User {
-
+	
  private Plan myPlantype;
  private int myMinitUsed;
  private int myLineNum;
  private String myName;
-	
+ 
  public User(Plan myPlan, int minitUsed, int lineNum, String name) {
   this.myPlantype=myPlan;
   this.myMinitUsed=minitUsed;
   this.myLineNum=lineNum;
   this.myName=name;
  }
-
  public Plan getMyPlantype() {
    return myPlantype;
  }
-
  public void setMyPlantype(Plan myPlantype) {
   this.myPlantype = myPlantype;
  }
-
  public int getMyinuteUsed() {
   return myMinitUsed;
  }
-
  public void setMyMiniUsed(int myMiniUsed) {
   this.myMinitUsed = myMiniUsed;
  }
-
  public int getMyLineNum() {
   return myLineNum;
  }
-
  public void setMyLineNum(int myLineNum) {
   this.myLineNum = myLineNum;
  }
-
  public String getMyName() {
   return myName;
  }
-
  public void setMyName(String myName) {
   this.myName = myName;
  }
-
 }

8cc0a050276263502212cc06780da2f5f04ac970
sh92 <sanghee.lee1992@gmail.com>
2016-04-03 07:36:01 +0900

    File 입출력으로 변

--- src/main/java/com/practical/billviewservice/App.java
+++ src/main/java/com/practical/billviewservice/App.java
@@ -1,21 +1,73 @@
 package com.practical.billviewservice;
-
+import java.io.*;
+import java.util.StringTokenizer;
 /**
  * Hello world!
  *
  */
 public class App {
-    private static final int USEDMINIUTE = 2000;
-    private static final int LINENUMBER = 3;
 
     private App() {
 
     }
 
     public static void main(String[] args) {
-
-        Plan myPlan = new Gold();
-        User myUser = new User(myPlan, USEDMINIUTE, LINENUMBER, "Hong-Gill-Dong");
+        
+        String plantype="";
+        int usedMiniute=0;
+        int lineNumber=0;
+        String userName="";
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader("userinfo.txt"));
+            String line = in.readLine();
+            while(line != null){
+            StringTokenizer parser = new StringTokenizer(line, " ");
+            
+                int cnt=0;
+                while(parser.hasMoreTokens()){
+                    String word = parser.nextToken().toUpperCase();
+                    if(cnt == 4)
+                        cnt=0;
+                    switch(cnt){
+                        case 0:
+                            plantype = word;
+                            break;
+                        case 1:
+                            usedMiniute = Integer.parseInt(word);
+                            break;
+                        case 2:
+                            lineNumber = Integer.parseInt(word);
+                            break;
+                        case 3:  
+                            userName = word;
+                            break;
+                        default:
+                    }
+                    cnt++;
+                }
+                line = in.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            
+        }finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        Plan myPlan = null;
+        if(plantype.toUpperCase().equals("GOLD")){
+            myPlan= new Gold();
+        }else if(plantype.toUpperCase().equals("SILVER")){
+            myPlan= new Silver();
+        }
+        
+        User myUser = new User(myPlan, usedMiniute, lineNumber, userName);
 
         Calculator myCalculator = new Calculator(myUser, myPlan);
         BillViewService billviewsystem = new BillViewService(myUser, myPlan, myCalculator);

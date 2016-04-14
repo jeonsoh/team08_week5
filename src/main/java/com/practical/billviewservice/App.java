e42d42011bbca0c15886b33c8b3d991e4fb4362b
sh92 <sanghee.lee1992@gmail.com>
2016-04-14 17:12:23 +0900

    합쳐보면 어떨까?

--- src/main/java/com/practical/billviewservice/App.java
+++ src/main/java/com/practical/billviewservice/App.java
@@ -4,6 +4,7 @@ package com.practical.billviewservice;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
@@ -13,9 +14,45 @@ import org.slf4j.LoggerFactory;
 public class App {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static Scanner scan;
+    public static void main(String[] args) throws IOException  {
+        if(args[0]==null)
+        {
+            User myUser = new User();
+            Plan myPlan;
+            scan = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
+            LOGGER.info("계획을 정해주세요 (GOLD? SILVER?) :");
+            String plan = scan.next();
+            if("GOLD".equalsIgnoreCase(plan)){
+                myPlan = new Gold();
+            }else if("SILVER".equalsIgnoreCase(plan)){
+                myPlan = new Silver();
+            }else{
+                throw new IllegalStateException("서비스 하지 않는 계획입니다.");
+            }
+            myUser.setMyPlantype(myPlan);
 
+            LOGGER.info("사용자가 이용 시간 ? :");
+            myUser.setMyMiniUsed(scan.nextInt());
+
+            LOGGER.info("사용자가 이용한 전화선 수? : ");
+            myUser.setMyLineNum(scan.nextInt());
+
+            LOGGER.info("사용자의 이름은? : ");
+            myUser.setMyName(scan.next());
+
+            Calculator myCalculator = new Calculator(myUser, myPlan);
+            BillViewService billviewsystem = new BillViewService(myUser, myPlan, myCalculator);
+            billviewsystem.showUser();
+            billviewsystem.showPlan();
+            billviewsystem.showCalculator();
+            billviewsystem.showTotalCalculator();
+        }else{
+            new App(args);
+        }
+    }
+    public App(String args[]) throws IOException{
         FileInputStream fis = null;
         FileOutputStream fos =null;
         String value = null;
@@ -52,7 +89,9 @@ public class App {
             }
         }
 
+        
     }
+
 
     private static void appRun(String line) {
         StringTokenizer parser = new StringTokenizer(line, " ");
@@ -62,7 +101,6 @@ public class App {
 
         while(parser.hasMoreTokens()){
              word = parser.nextToken().toUpperCase(); 
-             System.out.println(word);
              if("GOLD".equalsIgnoreCase(word)){
                  myPlan = new Gold();    
              }else if("SILVER".equalsIgnoreCase(word))
@@ -72,15 +110,12 @@ public class App {
              myUser.setMyPlantype(myPlan);
              
              word=parser.nextToken().toUpperCase();
-             System.out.println(word);
              myUser.setMyMiniUsed(Integer.valueOf(word));
              
              word=parser.nextToken().toUpperCase();
-             System.out.println(word);
              myUser.setMyLineNum(Integer.valueOf(word));
              
-             word=parser.nextToken().toUpperCase();
-             System.out.println(word);
+             word=parser.nextToken().toUpperCase();             
              myUser.setMyName(word);
         }
         Calculator myCalculator = new Calculator(myUser, myPlan);

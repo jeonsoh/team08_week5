d8bc1db385f6cc95f486ad580960803db85eb15a
sh92 <sanghee.lee1992@gmail.com>
2016-04-14 13:58:58 +0900

    App 변경

--- src/main/java/com/practical/billviewservice/App.java
+++ src/main/java/com/practical/billviewservice/App.java
@@ -5,10 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
     
     App() {
@@ -16,53 +13,62 @@ public class App {
     }
 
     public static void main(String[] args) {
-        User myUser = new User();
-        Plan myPlan = null;
-        int cnt=0;
+        
         try{
             BufferedReader in = new BufferedReader(new FileReader(args[0]));
             String line = in.readLine();
             while(line != null){
-            StringTokenizer parser = new StringTokenizer(line, " ");
-                while(parser.hasMoreTokens()){
-                    String word = parser.nextToken().toUpperCase();
-                    
-                    switch(cnt){
-                    case 0:
-                        if("GOLD".equalsIgnoreCase(word)){
-                        myPlan = new Gold();    
-                        }else if("SILVER".equalsIgnoreCase(word))
-                        {
-                            myPlan = new Silver();
-                        }        
-                        myUser.setMyPlantype(myPlan);
-                        break;
-                    case 1:   
-                        myUser.setMyMiniUsed(Integer.valueOf(word));
-                        break;
-                    case 2:
-                        myUser.setMyLineNum(Integer.valueOf(word));
-                        break;
-                    case 3:
-                        myUser.setMyName(word);
-                        break;
-                    }
-                    cnt++;                    
-                    if(cnt==4)
-                        cnt=0;
-                }
+                User myUser = new User();
+                Plan myPlan = null;
+                int cnt=0;
+                StringTokenizer parser = new StringTokenizer(line, " ");
+                myPlan = appRun(myUser, myPlan, cnt, parser);
                 line = in.readLine();
+                Calculator myCalculator = new Calculator(myUser, myPlan);
+                BillViewService billviewsystem = new BillViewService(myUser, myPlan, myCalculator);
+                billviewsystem.showUser();
+                billviewsystem.showPlan();
+                billviewsystem.showCalculator();
+                billviewsystem.showTotalCalculator();
             }
             in.close();
             
         }catch(IOException e){
             System.out.println(e);
         }
-        Calculator myCalculator = new Calculator(myUser, myPlan);
-        BillViewService billviewsystem = new BillViewService(myUser, myPlan, myCalculator);
-        billviewsystem.showUser();
-        billviewsystem.showPlan();
-        billviewsystem.showCalculator();
-        billviewsystem.showTotalCalculator();
+      
+    }
+
+    private static Plan appRun(User myUser, Plan myPlan, int cnt, StringTokenizer parser) {
+        while(parser.hasMoreTokens()){
+            String word = parser.nextToken().toUpperCase();
+            
+            switch(cnt){
+            case 0:
+                if("GOLD".equalsIgnoreCase(word)){
+                myPlan = new Gold();    
+                }else if("SILVER".equalsIgnoreCase(word))
+                {
+                    myPlan = new Silver();
+                }        
+                myUser.setMyPlantype(myPlan);
+                break;
+            case 1:   
+                myUser.setMyMiniUsed(Integer.valueOf(word));
+                break;
+            case 2:
+                myUser.setMyLineNum(Integer.valueOf(word));
+                break;
+            case 3:
+                myUser.setMyName(word);
+                break;
+            default:
+                    
+            }
+            cnt++;                    
+            if(cnt==4)
+                cnt=0;
+        }
+        return myPlan;
     }
 }

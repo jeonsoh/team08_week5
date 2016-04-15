c4e9453b194eed9f8489d58269a661143ba062a4
sh92 <sanghee.lee1992@gmail.com>
2016-04-15 13:20:06 +0900

    Exception 처리 제

--- src/main/java/com/practical/billviewservice/User.java
+++ src/main/java/com/practical/billviewservice/User.java
@@ -34,11 +34,8 @@ public class User {
     }
 
     public void setMyMiniUsed(int myMiniUsed){
-        if(myMiniUsed < 0){
-            throw new IllegalStateException( "사용 시간은 음수일 수 없습니다." );
-        }else{
-            this.myMinitUsed = myMiniUsed;
-        }
+       
+        this.myMinitUsed = myMiniUsed;
     }
 
     public int getMyLineNum() {
@@ -46,22 +43,14 @@ public class User {
     }
 
     public void setMyLineNum(int myLineNum){
-        if(myLineNum <= 0){
-            throw new IllegalStateException("라인의 수는 1 이상이여야 합니다.");
-        }else{
-            this.myLineNum = myLineNum;
-        }
+        this.myLineNum = myLineNum;
     }
 
     public String getMyName() {
         return myName;
     }
 
-    public void setMyName(String myName) {
-        if(myName == null){
-            throw new IllegalStateException("이름이 입력되지 않았습니다.");
-        }else{
-            this.myName = myName;
-        }
+    public void setMyName(String myName) {       
+       this.myName = myName;
     }
 }

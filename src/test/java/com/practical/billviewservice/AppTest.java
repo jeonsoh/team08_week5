3e7b18db0432d205132b17c403609e09b553ffb8
sh92 <sanghee.lee1992@gmail.com>
2016-04-15 15:49:28 +0900

    테스트케이스 추가

--- src/test/java/com/practical/billviewservice/AppTest.java
+++ src/test/java/com/practical/billviewservice/AppTest.java
@@ -51,12 +51,18 @@ public class AppTest extends TestCase {
     } 
     @Test
     public void testErrorApp() throws Exception {
-        String[] userfile= new String[4];
+        String[] userfile= new String[10];
         
-        userfile[0] = "--file";
-        userfile[1] = "user5.txt";
-        userfile[2] = "user10.txt";
-        userfile[3] = "user5.txt";
+        userfile[0] = "user1.txt";
+        userfile[1] = "user2.txt";
+        userfile[2] = "user3.txt";
+        userfile[3] = "user4.txt";
+        userfile[4] = "user5.txt";
+        userfile[5] = "user6.txt";
+        userfile[6] = "user7.txt";
+        userfile[7] = "user8.txt";
+        userfile[8] = "user9.txt";
+        userfile[9] = "user10.txt";
         
         App.main(userfile);
        
@@ -66,13 +72,18 @@ public class AppTest extends TestCase {
     
     @Test
     public void testFile() throws Exception {
-        String[] userfile= new String[4];
+        String[] userfile= new String[10];
         
-        userfile[0] = "--file";
-        userfile[1] = "user1.txt";
-        userfile[2] = "user2.txt";
-        userfile[3] = "user3.txt";
-        
+        userfile[0] = "user11.txt";
+        userfile[1] = "user12.txt";
+        userfile[2] = "user13.txt";
+        userfile[3] = "user14.txt";
+        userfile[4] = "user15.txt";
+        userfile[5] = "user16.txt";
+        userfile[6] = "user17.txt";
+        userfile[7] = "user18.txt";
+        userfile[8] = "user19.txt";
+        userfile[9] = "user20.txt";
         App.testMain("/target",userfile);
        
         assertTrue(true);
@@ -80,12 +91,18 @@ public class AppTest extends TestCase {
     
     @Test
     public void testFileError() throws Exception {
-        String[] userfile= new String[4];
+        String[] userfile= new String[10];
         
-        userfile[0] = "user1.txt";
-        userfile[1] = "user10.txt";
-        userfile[2] = "user8.txt";
-        userfile[3] = "user9.txt";
+        userfile[0] = "user11.txt";
+        userfile[1] = "user12.txt";
+        userfile[2] = "user13.txt";
+        userfile[3] = "user14.txt";
+        userfile[4] = "user15.txt";
+        userfile[5] = "user16.txt";
+        userfile[6] = "user17.txt";
+        userfile[7] = "user18.txt";
+        userfile[8] = "user19.txt";
+        userfile[9] = "user20.txt";
         
         App.testMain("/target",userfile);
        

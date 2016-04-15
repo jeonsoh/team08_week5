366ef57ab86d18ef0c0c2b376cd279b518d5391b
sh92 <sanghee.lee1992@gmail.com>
2016-04-15 14:58:24 +0900

    Test경우 추

--- src/test/java/com/practical/billviewservice/AppTest.java
+++ src/test/java/com/practical/billviewservice/AppTest.java
@@ -75,6 +75,20 @@ public class AppTest extends TestCase {
        
         assertTrue(true);
     }
+    
+    @Test
+    public void testFileError() throws Exception {
+        String[] userfile= new String[4];
+        
+        userfile[0] = "user1.txt";
+        userfile[1] = "user10.txt";
+        userfile[2] = "user8.txt";
+        userfile[3] = "user9.txt";
+        
+        App.testMain("/target",userfile);
+       
+        assertTrue(true);
+    }
 
    
 }

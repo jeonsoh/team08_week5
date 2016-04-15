2a0a15b70d7db652f83d8eb7d5ae5b310c1a9cd2
sh92 <sanghee.lee1992@gmail.com>
2016-04-15 14:29:28 +0900

    Logger삭제

--- src/test/java/com/practical/billviewservice/AppSilverTest.java
+++ src/test/java/com/practical/billviewservice/AppSilverTest.java
@@ -4,8 +4,7 @@ package com.practical.billviewservice;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 import junit.framework.TestCase;
 
@@ -19,7 +18,7 @@ public class AppSilverTest extends TestCase {
     String name = "홍길동";
     Plan myPlan = null;
     User myUser = null;
-    private static final Logger LOGGER = LoggerFactory.getLogger(AppGoldTest.class);
+
     Exception ex;
     
     @Before
